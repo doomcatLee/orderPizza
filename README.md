ae14853165012ff2c136a3902e77ee12d1ceefa7
Alex Lee <doomcat9167@gmail.com>
2017-03-03 10:57:10 -0800

    add back end objects for Pizza and User

---
+++ README.md
@@ -0,0 +1,55 @@
+
+# Order Pizza
+
+#### Mar 3 2017. v.1.0
+
+#### By _Alex Lee_
+
+## Description
+
+Allows user to make a pizza. User chooses multiple toppings and size which they will see the cost of the pizza.
+
+## Specifications
+
+|Behavior|Input|Output|
+|--|--|--|
+|User picks a topping, returns that topping to the Pizza object| chooseTopping() | "Pepperoni"|
+|User picks a size, returns that size to the pizza object | chooseSize() | "medium"|
+|Calculate the cost of the pizza depending on size and number of toppings | calculateCost()|"$40"|
+
+
+
+
+
+## Setup/Installation Requirements
+```
+cd /desiredLocation
+```
+```
+git clone https://github.com/doomcatlee/orderPizza
+```
+```
+cd orderPizza
+```
+```
+open index.html
+```
+
+## Known Bugs
+
+Known bugs here
+
+## Support and contact details
+
+Contact information here
+
+## Technologies Used
+
+_bash_
+_git_
+
+### License
+
+*This software is licensed under the MIT license.*
+
+Copyright (c) 2017 Alex Lee
---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+// Back-End
+function Customer(name, address, pizzaArray, totalCost) {
+  this.name = name;
+  this.address = address;
+  this.pizzaArray = [];
+  this.totalCost = 0;
+}
+
+Customer.prototype.addPizza = function(pizza) {
+  pizza.pizzaCost = 10;
+  $("#pizzaInfo").append("<ul>" + "<li>Pizza toppings: <span id='topping'></span></li>" + "<li>Pizza size: <span id='size'></span></li>" + "<li>Cost: <span id='cost'></span></li>" + "</ul>");
+}
+
+
+function Pizza(toppingArray, size, pizzaCost) {
+  this.toppingArray = [];
+  this.size = size;
+  this.pizzaCost = 10;
+}
+
+//push one topping to the toppingArray each call
+Pizza.prototype.chooseTopping = function(topping) {
+  this.toppingArray.push(topping);
+}
+
+//add size to the Pizza object
+Pizza.prototype.chooseSize = function(size) {
+  this.size = size;
+}
+
+Pizza.prototype.calculateCost = function() {
+  for (var i = 0; i < this.toppingArray.length; i++) {
+    this.pizzaCost += 3;
+  }
+  if (this.size === "Small") {
+    this.pizzaCost += 5;
+  } else if (this.size === "Medium") {
+    this.pizzaCost += 10;
+  } else if (this.size === "Large") {
+    this.pizzaCost += 15;
+  } else if (this.size === "Extra large") {
+    this.pizzaCost += 20;
+  } else {
+    console.log("do nothing");
+  }
+  return this.pizzaCost;
+}
+
+
+var pizza = new Pizza();
+var user = new Customer();
+//jQuery
+$(function() {
+
+  $("form#topping").submit(function() {
+    event.preventDefault();
+    var toppingArray = [];
+    $('#topping input:checked').each(function() {
+      pizza.chooseTopping($(this).val());
+    });
+  }); //topping form
+
+  $("form#size").submit(function() {
+    event.preventDefault();
+    pizza.size = $("input:radio[name=optradio]:checked").val();
+    (user.pizzaArray).push(pizza);
+    user.totalCost += pizza.calculateCost();
+
+    pizza.toppingArray.forEach(function(each) {
+      $("span#topping").last().append(each + ", ");
+    });
+    $("span#size").last().append(pizza.size)
+    $("span#cost").last().append(pizza.pizzaCost)
+    $("span#total").text(user.totalCost)
+  }); //size form
+
+  $("button#cost").click(function() {}); //
+  $("button#addPizza").click(function() {
+    user.addPizza(pizza);
+    pizza = new Pizza();
+  }); //
+
+  $("form#order").submit(function() {
+    event.preventDefault();
+    var name = $("#name").val();
+    var streetAddress = "";
+    for (var i = 0; i < 3; i++) {
+      streetAddress += ($("#" + i).val()) + " ";
+    }
+    user.name = name;
+    user.address = streetAddress;
+    $("span#name").text(name);
+    $("span#address").text(streetAddress);
+  }); //order form
+
+}); //jQuery
+//jQuery
